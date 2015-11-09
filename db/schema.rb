72c2a29bfd126db99ac5eaebf46b7ec9a69a2e51
Danilo <danilo.diaz.ga@gmail.com>
2015-11-09 03:34:32 -0300

    edicion realizada en arch, prueba

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151104202736) do
+ActiveRecord::Schema.define(version: 20151103211246) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -24,12 +24,6 @@ ActiveRecord::Schema.define(version: 20151104202736) do
 
   add_index "articles", ["user_id"], name: "index_articles_on_user_id"
 
-  create_table "categories", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "comments", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "article_id"
@@ -37,6 +31,9 @@ ActiveRecord::Schema.define(version: 20151104202736) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_index "comments", ["article_id"], name: "index_comments_on_article_id"
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -49,6 +46,8 @@ ActiveRecord::Schema.define(version: 20151104202736) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "name"
+    t.string   "permission_level"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
   end
